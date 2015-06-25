e4711885654635165176f9b220610ba45a748351
BenlyXu <xubenling0918@gmail.com>
2015-06-25 15:51:19 +0800

    Add shell sort.

--- src/com/changyou/demo/datastructure/sort/ShellSort.java
+++ src/com/changyou/demo/datastructure/sort/ShellSort.java
@@ -6,10 +6,28 @@ package com.changyou.demo.datastructure.sort;
  * @date Jun 24, 2015 4:55:29 PM
  */
 public class ShellSort {
+	
+	public void sort(int[] arr) {
+		for (int increment = arr.length / 2; increment > 0; increment /= 2) {
+			System.out.println("increment=" + increment);
+			for (int i = 0; i < arr.length; i += increment) {
+				System.out.println("i=" + i);
+				for (int j = i; j > 0; j -= increment) {
+					if (arr[j] < arr[j - increment]) {
+						ArraysUtil.swap(arr, j, j - increment);
+					}
+				}
+				ArraysUtil.print(arr);
+			}
+		}
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr = {9, 1, 5, 8, 3, 7, 4, 6, 2};
+		ShellSort ss = new ShellSort();
+		ss.sort(arr);
+//		ArraysUtil.print(arr);
 	}
 
 }
