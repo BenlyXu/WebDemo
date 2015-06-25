f8fca62e773598be20451b4f820b90971aaec4cd
BenlyXu <xubenling0918@gmail.com>
2015-06-25 09:44:21 +0800

    Three different ways to swap value.

--- src/com/changyou/demo/datastructure/sort/ArraysUtil.java
+++ src/com/changyou/demo/datastructure/sort/ArraysUtil.java
@@ -6,9 +6,20 @@ public final class ArraysUtil {
 
 	public static void swap(int[] arr, int i, int j) {
 		if (i != j) {
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
+			// 方法一：定义缓存变量
+//			int temp = arr[i];
+//			arr[i] = arr[j];
+//			arr[j] = temp;
+			
+			// 方法二：相加后再减，可能会溢出
+//			arr[i] = arr[i] + arr[j];
+//			arr[j] = arr[i] - arr[j];
+//			arr[i] = arr[i] - arr[j];
+			
+			// 方法三：与或
+			arr[i] = arr[i] ^ arr[j];
+			arr[j] = arr[i] ^ arr[j];
+			arr[i] = arr[i] ^ arr[j];
 		}
 	}
 	
