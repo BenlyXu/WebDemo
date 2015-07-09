433aefc24e4b03c31bba36221405735896631d09
Benly <xubenling0918@gmail.com>
2015-07-09 21:32:05 +0800

    Bit operation.

--- src/com/changyou/demo/points/question/Power.java
+++ src/com/changyou/demo/points/question/Power.java
@@ -11,13 +11,32 @@ public class Power {
 		
 		return Math.pow(base, exponent);
 	}
+	
+	// a^n = a^(n/2)*a^(n/2), n为偶数
+	// a^n = a^((n-1)/2)*a^((n-1)/2)*a, n为奇数
+	public double power2(double base, int exponent) {
+		if (0 == exponent) {
+			return 1;
+		}
+		if (1 == exponent) {
+			return base;
+		}
+		double result = power2(base, exponent >> 1);  // 右移一位相当于除以2，注意exponent为整形，整除取整
+		result *= result;
+		if ((exponent & 0x1) == 1) {  // 与1位与判断奇偶
+			result *= base;
+		}
+		return result;
+	}
 
 	public static void main(String[] args) {
 		Power p = new Power();
-		System.out.println(p.power(2, 3));
-		System.out.println(p.power(1, 10));
-		System.out.println(p.power(0, 1));
+//		System.out.println(p.power(2, 3));
+//		System.out.println(p.power(1, 10));
+//		System.out.println(p.power(0, 1));
 		
+		System.out.println(p.power2(2, 5));
+//		System.out.println(p.power2(2, 4));
 	}
 
 }
