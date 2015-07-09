2592bcc23777058f70eb6b09fc028b711719324b
Benly <xubenling0918@gmail.com>
2015-07-09 21:33:25 +0800

    Convert string to integer.

---
+++ src/com/changyou/demo/points/question/StringToInt.java
@@ -0,0 +1,24 @@
+package com.changyou.demo.points.question;
+
+public class StringToInt {
+	
+	
+	public static int s2i(String s) throws Exception {
+		if (null == s || 0 == s.length()) {
+			throw new IllegalArgumentException("Illegal number string");
+		}
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		String s = "12345";
+		int number = 0;
+		char[] chars = s.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			number = number * 10 + chars[i] - '0';
+		}
+		System.out.println(number);
+	}
+
+}
