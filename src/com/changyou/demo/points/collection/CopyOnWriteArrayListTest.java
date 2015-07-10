dd37f4481cf0a7777b554c2f28beaa54af42af84
BenlyXu <xubenling0918@gmail.com>
2015-07-10 16:14:25 +0800

    Use CopyOnWriteArrayList when modify list on iterating.

---
+++ src/com/changyou/demo/points/collection/CopyOnWriteArrayListTest.java
@@ -0,0 +1,37 @@
+package com.changyou.demo.points.collection;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteArrayListTest {
+
+	public static void main(String[] args) {
+		List<String> list = new CopyOnWriteArrayList<String>();
+		list.add("a");
+		list.add("b");
+		list.add("java");
+		list.add("c");
+		
+		for (String string : list) {
+			System.out.println(string);
+		}
+		
+		/*
+		 * 如果在迭代的时候对列表进行改变，应使用<code>CopyOnWriteArrayList</code>
+		 */
+		for (String string : list) {
+			if ("java".equals(string)) {
+				list.remove(string);
+				list.add("d");
+				list.add("e");
+			}
+		}
+		
+		System.out.println();
+		for (String string : list) {
+			System.out.println(string);
+		}
+		
+	}
+
+}
