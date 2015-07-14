d21b59399133a724e6af37e2395c634a6b681067
Benly <Benly@Superben-PC>
2015-07-14 22:38:58 +0800

    Exception extends relation.

---
+++ src/com/demo/points/exception/Annoyance.java
@@ -0,0 +1,10 @@
+package com.demo.points.exception;
+
+public class Annoyance extends Exception {
+	
+	/**
+	 * Serial version UID
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/com/demo/points/exception/Sneeze.java
@@ -0,0 +1,10 @@
+package com.demo.points.exception;
+
+public class Sneeze extends Annoyance {
+
+	/**
+	 * Serial version UID
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
