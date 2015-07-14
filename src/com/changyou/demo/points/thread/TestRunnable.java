b7f8d127013215341a82c443e45d3a0f4001dc75
BenlyXu <xubenling0918@gmail.com>
2015-07-14 10:20:38 +0800

    Transfer data to thread and how to use join() method.

--- src/com/changyou/demo/points/thread/TestRunnable.java
+++ src/com/changyou/demo/points/thread/TestRunnable.java
@@ -2,9 +2,41 @@ package com.changyou.demo.points.thread;
 
 public class TestRunnable implements Runnable {
 
+	private String val;
+	
+	public TestRunnable() {
+	}
+	
+	public TestRunnable(String val) {
+		this.val = val;
+	}
+	
 	@Override
 	public void run() {
-		System.out.println(this.getClass().getName());
+		System.out.println(val);
+//		System.out.println(this.getClass().getName());
+	}
+	
+	public static void main(String[] args) {
+		// 1.通过构造方法传值
+//		TestRunnable runnable = new TestRunnable("hello world");
+//		Thread thread = new Thread(runnable);
+//		thread.start();
+//		try {
+//			thread.join();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+		// 2.通过setter方法传值
+//		TestRunnable runnable = new TestRunnable();
+//		runnable.val = "hello world";
+//		Thread thread = new Thread(runnable);
+//		thread.start();
+//		try {
+//			thread.join();
+//		} catch (Exception e) {
+//		}
+		// 3.通过回调函数传值 http://www.blogjava.net/nokiaguy/archive/2009/03/16/260131.html
 	}
 
 }
